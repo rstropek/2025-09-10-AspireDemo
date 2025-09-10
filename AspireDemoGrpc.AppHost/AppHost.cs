17259ef01119f59824f38aa5405659a60f507f95
Rainer <rainer@software-architects.at>
2025-09-10 10:03:27 +0200

    Implement gRPC server with Greeter service and integrate with WebApi

--- AspireDemoGrpc.AppHost/AppHost.cs
+++ AspireDemoGrpc.AppHost/AppHost.cs
@@ -1,6 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var grpcServer = builder.AddProject<Projects.GrpcServer>("grpcserver");
+
 var webapi = builder.AddProject<Projects.WebApi>("webapi")
+    .WithReference(grpcServer)
     .WithReplicas(2);
 
 builder.Build().Run();
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,10 +1,39 @@
+using System.Diagnostics;
+using GrpcDemo;
+
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+foreach (System.Collections.DictionaryEntry envVar in Environment.GetEnvironmentVariables())
+{
+    Console.WriteLine($"{envVar.Key}={envVar.Value}");
+}
+
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
+
+builder.Services.AddGrpcClient<Greeter.GreeterClient>(o =>
+{
+    o.Address = new Uri("http://grpcserver");
+});
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 app.UseHttpsRedirection();
 
+var source = new ActivitySource("WebApi");
+
 app.MapGet("/ping", () => new { Message = "pong" });
+
+app.MapGet("/callGreeter", async (Greeter.GreeterClient client) =>
+{
+    HelloReply reply;
+    using (var activity = source.StartActivity("Calling gRPC Greeter"))
+    {
+        reply = await client.SayHelloAsync(new HelloRequest { Name = "WebApi" });
+    }
+
+    return Results.Ok(new { reply.Message });
+});
 
 app.Run();
